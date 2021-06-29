17c1acda2a1eaa0595ba1ed0a06ac04350a34d75
taowc1 <taowc1@lenovo.com>
2021-06-29 15:53:41 +0800

    eureka demo

---
+++ spring-cloud-server/eureka-server/src/main/java/com/chinaoly/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.chinaoly.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableFeignClients(basePackages = { "com.chinaoly" })
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
